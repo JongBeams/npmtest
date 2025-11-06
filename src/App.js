52d8df219f28dff9cdac8f72b7e018a82438c4fb
JongBeams <wwhow2003@gmail.com>
2025-11-06 13:55:30 +0900

    react 함수형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent/>
+      <FourthComponent></FourthComponent>
     </div>
   );
 }
+
+function FirstComponent(){
+  return (
+    <div className='First Component'>
+      First Component
+    </div>
+  );
+}
+
+function SecondComponent(){
+  return (
+    <div className='Second Component'>
+      Second Component
+    </div>
+  );
+}
+
+class ThirdComponent extends Component{
+    render(){
+    return (
+      <div className='Third Component'>
+        Third Component
+      </div>
+    );
+  }
+}
+
+class FourthComponent extends Component{
+    render(){
+    return (
+      <div className='Fourth Component'>
+        Fourth Component
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
